c29bcf80ac6fb688613289bbeca6433a08acc2f1
Nikolay <3kaal6@gmail.com>
2024-05-13 22:36:21 +1000

    исправил DAG dag_create_table.py добавил template_searchpath

--- dags/dag_create_table.py
+++ dags/dag_create_table.py
@@ -1,69 +1,62 @@
-from airflow.decorators import dag
 import pendulum
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow import DAG
 
+import os
 
-@dag(
+dag = DAG(
     dag_id="create_tables",
     start_date=pendulum.datetime(2024, 5, 9),
     schedule="@once",
     catchup=True,
     tags=["upload"],
+    template_searchpath=f'{os.path.dirname(__file__)}/sql/create_table'
 )
-def create_table():
-    """
-    Создает таблицы в базе данных PostgreSQL для хранения информации о предметах, героях,
-    профессиональных командах и матчах.
 
-    Выполняет последовательное создание таблиц с использованием оператора SQLExecuteQueryOperator.
-    Созданные таблицы: items, heroes, pro_teams, matches, player_matches.
+create_table_items = SQLExecuteQueryOperator(
+    task_id="create_table_items",
+    conn_id="postgres",
+    autocommit=True,
+    database="postgres",
+    sql="create_table_items.sql",
+    dag=dag,
+)
 
-    Возвращает:
-    None
-    """
+create_table_heroes = SQLExecuteQueryOperator(
+    task_id="create_table_heroes",
+    conn_id="postgres",
+    autocommit=True,
+    database="postgres",
+    sql="create_table_heroes.sql",
+    dag=dag,
+)
 
-    create_table_items = SQLExecuteQueryOperator(
-            task_id="create_table_items",
-            conn_id="postgres",
-            autocommit=True,
-            database="postgres",
-            sql="sql/create_table/create_table_items.sql"
-        )
+create_table_pro_teams = SQLExecuteQueryOperator(
+    task_id="create_table_pro_teams",
+    conn_id="postgres",
+    autocommit=True,
+    database="postgres",
+    sql="create_table_pro_teams.sql",
+    dag=dag,
+)
 
-    create_table_heroes = SQLExecuteQueryOperator(
-            task_id="create_table_heroes",
-            conn_id="postgres",
-            autocommit=True,
-            database="postgres",
-            sql="sql/create_table/create_table_heroes.sql",
-        )
+create_table_pro_matches = SQLExecuteQueryOperator(
+    task_id="create_table_matches",
+    conn_id="postgres",
+    autocommit=True,
+    database="postgres",
+    sql="create_table_matches.sql",
+    dag=dag,
+)
 
-    create_table_pro_teams = SQLExecuteQueryOperator(
-            task_id="create_table_pro_teams",
-            conn_id="postgres",
-            autocommit=True,
-            database="postgres",
-            sql="sql/create_table/create_table_pro_teams.sql",
-        )
+create_table_pro_players = SQLExecuteQueryOperator(
+    task_id="create_table_player_matches",
+    conn_id="postgres",
+    autocommit=True,
+    database="postgres",
+    sql="create_table_player_matches.sql",
+    dag=dag,
+)
 
-    create_table_pro_matches = SQLExecuteQueryOperator(
-            task_id="create_table_matches",
-            conn_id="postgres",
-            autocommit=True,
-            database="postgres",
-            sql="sql/create_table/create_table_matches.sql",
-        )
-
-    create_table_pro_players = SQLExecuteQueryOperator(
-            task_id="create_table_player_matches",
-            conn_id="postgres",
-            autocommit=True,
-            database="postgres",
-            sql="sql/create_table/create_table_player_matches.sql",
-        )
-
-    (create_table_items >> create_table_heroes >> create_table_pro_teams >> create_table_pro_matches >>
-     create_table_pro_players)
-
-
-dag = create_table()
+(create_table_items >> create_table_heroes >> create_table_pro_teams >> create_table_pro_matches >>
+ create_table_pro_players)
